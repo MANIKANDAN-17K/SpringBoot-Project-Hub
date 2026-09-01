4cd671fa53dd6493863011322c4d9aaef3266278
MANIKANDAN K <160108937+MANIKANDAN-17K@users.noreply.github.com>
2026-09-01 21:39:16 +0530

    Add files via upload

---
+++ resume-webapp/src/main/java/com/manikandan/resume/ResumeWebappApplication.java
@@ -0,0 +1,11 @@
+package com.manikandan.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWebappApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeWebappApplication.class, args);
+	}
+}
